297d084d3089c2a9d836e38c5cebbb3f31adc6c7
Jacinto <jgonzalezr@unemi.edu.ec>
2023-01-20 00:40:30 -0500

    varias modificaciones

--- proyectoQrLogin/cuentaApp/urls.py
+++ proyectoQrLogin/cuentaApp/urls.py
@@ -3,6 +3,8 @@ from django.views.generic import TemplateView
 
 from . import views
 
+#urls para que la pagina primera de cargar sea el template de login
+#y luego redireccione al template home
 urlpatterns = [
     path('', views.homepage, name='home'),
     path('login/', views.loginPage, name='login'),
--- proyectoQrLogin/generarQr/urls.py
+++ proyectoQrLogin/generarQr/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+#url para la enlazar a la aplicacion generarQr
 urlpatterns = [
     path('', views.generate, name='generarQr'),
 ]
