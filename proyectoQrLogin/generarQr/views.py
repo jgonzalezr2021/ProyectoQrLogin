772c300b153af6d59fb583667a51e51bf20df419
Jacinto <jgonzalezr@unemi.edu.ec>
2023-01-20 00:37:02 -0500

    varias modificaciones

--- proyectoQrLogin/generarQr/views.py
+++ proyectoQrLogin/generarQr/views.py
@@ -4,3 +4,11 @@ from django.utils.encoding import smart_str
 from django.shortcuts import render
 from barcode.writer import ImageWriter
 from django.http import HttpResponse
+
+def generate_file(barcode_file):
+    # output = HttpResponse(content_type="image/jpeg")
+    output = HttpResponse(content_type="application/force-download")
+    barcode_file.save(output, "JPEG")
+    output['Content-Disposition'] = 'attachment; filename=%s' % smart_str('barcode.jpg')
+    output['X-Sendfile'] = smart_str('barcode.jpg')
+    return output
