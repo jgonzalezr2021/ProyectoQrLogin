f3d6f16d02ee1374f3d6269e27b76853cc9f17b0
Jacinto <jgonzalezr@unemi.edu.ec>
2023-01-20 00:33:52 -0500

    varias modificaciones

--- proyectoQrLogin/generarQr/views.py
+++ proyectoQrLogin/generarQr/views.py
@@ -1,3 +1,6 @@
+import qrcode
+import barcode
+from django.utils.encoding import smart_str
 from django.shortcuts import render
-
-# Create your views here.
+from barcode.writer import ImageWriter
+from django.http import HttpResponse
