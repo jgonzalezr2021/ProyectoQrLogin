6d9e3641c91968b5194c7c0634b44fec198f6b0a
Jacinto <jgonzalezr@unemi.edu.ec>
2023-01-20 00:16:52 -0500

    vista detectar modificando

--- proyectoQrLogin/detectarQr/views.py
+++ proyectoQrLogin/detectarQr/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+import os
+import time
+from django.shortcuts import render
+from datetime import datetime
+from django.http import JsonResponse, StreamingHttpResponse
+from PIL import Image
+from pyzbar.pyzbar import decode
+from utils.camera_streaming_widget import CameraStreamingWidget
 
-# Create your views here.
+# alimentacion de la camara
+def camera_feed(request):
+    stream = CameraStreamingWidget()
+    frames = stream.get_frames()
+
+    #
