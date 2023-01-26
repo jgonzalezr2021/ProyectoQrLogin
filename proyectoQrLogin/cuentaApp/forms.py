4ec964ebfbbabe9a569aa897710513b437b859a6
Jacinto <jgonzalezr@unemi.edu.ec>
2023-01-25 20:59:24 -0500

    modificando tablas

--- proyectoQrLogin/cuentaApp/forms.py
+++ proyectoQrLogin/cuentaApp/forms.py
@@ -1,10 +1,9 @@
-from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 
 
-
-from django import forms
 
 class CreateUserForm(UserCreationForm):
     class Meta:
